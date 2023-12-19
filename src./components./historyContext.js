4125850066fc4e96b8bb55a521c2a86a355dd844
tomerkl22 <94317058+tomerkl22@users.noreply.github.com>
2023-12-19 22:07:21 +0000

    Update historyContext.js - Mongodb

--- src./components./historyContext.js
+++ src./components./historyContext.js
@@ -3,40 +3,22 @@ import { useState } from "react";
 import axios from 'axios';
 const HistoryContext = createContext(); 
 
+
 export function HistoryProvider ({children})
 {
     const[historyList, setHistoryList] = useState([]);
+
     const addToHistory = (item) =>{
         setHistoryList ((prevState) => [...prevState, item])
-        //console.log(historyList);
-    };
-
+    };  
+   
     useEffect(()=>{
-        axios.get('http://localhost:8080/').then(response => {
-            let data =[];
-            console.log(response.data);
-            response.data.map(element => {
-            data.push(element);
-            });
-            setHistoryList([...historyList,data]);
-        })
-        // I was to do keep in the localStorage...
-        // let data = JSON.parse(localStorage.getItem("HISTORY"));
-        // if(data)
-        // {
-        //     setHistoryList(data)
-        // }
-    }, [])
-    
-    useEffect(()=>{
-        if(historyList.length > 0){
-            //  localStorage.setItem("HISTORY", JSON.stringify(historyList));
-            //  const lastGame = historyList[historyList.length - 1];
-             axios.post('http://localhost:8080/', historyList);
-            // const json = JSON.stringify({ answer: 42 });
-            // const res = axios.post('http://localhost:3001/', json);
-
-        }
+        try {
+            const lastElement = historyList[historyList.length - 1];
+            const response = axios.put(`http://localhost:3000/update-data/`, lastElement);
+          } catch (error) {
+            console.error('Error updating data:', error.response ? error.response.data : error.message);
+          }
     }, [historyList])
 
     // everything inside this provider will have access to the context
